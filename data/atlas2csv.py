5dec4d5c5e608d22f64809788680944535e2cbc9
Simeon Nedkov <simeon@ndkv.nl>
2015-10-28 11:14:20 +0100

    add raw data finder

--- data/atlas2csv.py
+++ data/atlas2csv.py
@@ -3,16 +3,48 @@ import requests
 import codecs
 import unicodecsv as csv
 import re
+from bs4 import BeautifulSoup as BS
+
+def find_data(url, layer):
+	wfs_url = re.sub(re.compile(ur'wms', re.IGNORECASE), 'wfs', url)
+
+	error = ""
+
+	if 'wfs' in wfs_url or 'ows' in wfs_url:
+		parameters = "SERVICE=WFS&VERSION=1.1.0&REQUEST=GetFeature&TYPENAME=%s&OUTPUTFORMAT=json&SRSNAME=EPSG:4326" % layer
+		limit = "&maxFeatures=1"
+
+		if wfs_url[-1] == "?":
+			wfs_url += parameters
+		else:
+			wfs_url += "?" + parameters
+		
+		r = requests.get(wfs_url + limit)
+		if r.status_code == 200:
+			try:
+				a = r.json()
+				return wfs_url, error 
+
+			except ValueError:
+				soup = BS(r.text, 'xml')
+				error = soup.find('ExceptionText').string
+
+				return wfs_url, error
+		else:
+			error = "HTTP 404"
+			return wfs_url, error
+	else:
+		return url, error
+
 
 # get ank-themas.json from initial ANK map viewer requests
 # http://www.atlasnatuurlijkkapitaal.nl/kaarten?p_p_id=atlasMap_WAR_atlasfrontendportlet_INSTANCE_FotTSS5BXAUh&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=getThemas&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1
-
 with open('ank-themas.json', 'r') as f:
 	themes = load(f)['atlas.themas']
 
 ank_f = open('ank.csv', 'w')
 writer = csv.writer(ank_f, delimiter=';')
-writer.writerow(["naam", "samenvatting", "thema", "thema samenvatting", "onderwerp", "type", "eigenaar", "service type", "data url", "kaart url", "laagnaam"])
+writer.writerow(["naam", "samenvatting", "thema", "onderwerp", "eigenaar", "data url", "data url error", "kaart url", "laagnaam"])
 
 # broken_f = codecs.open('ank-broken.csv', 'w', encoding='utf8')
 
@@ -29,12 +61,12 @@ for theme in themes:
 		name = indicator['naam']
 		summary = indicator['samenvatting']
 		subject = indicator['onderwerp']
-		type_ = indicator['type']
+		# type_ = indicator['type']
 
 		map_url = indicator['uiKaartProxy']['mapUrl']
 		layer = indicator['uiKaartProxy']['layerName']
 		data_owner = indicator['uiKaartProxy']['bronhouderNaam']
-		service_type = indicator['uiKaartProxy']['serviceType']
+		# service_type = indicator['uiKaartProxy']['serviceType']
 
 		#test if service returns sane Capabilities document
 		# TODO
@@ -61,10 +93,11 @@ for theme in themes:
 			if len(map_info) > 0:
 				url_re = re.search('(?<=<\/strong>)http:\/\/[^c].+?(?=<br \/>)', map_info)
 
-				print re.findall('(?<=<\/strong>)http:\/\/[^c].+?(?=<br \/>)', map_info)
+				# print re.findall('(?<=<\/strong>)http:\/\/[^c].+?(?=<br \/>)', map_info)
 
 				if url_re != None:
 					url = url_re.group(0)
+					data_url, data_url_error = find_data(url, layer)
 				else:
 					print "Failed to find URL... "
 					failed_url.append([name, theme_name, 'no data url found in info tab'])
@@ -79,7 +112,7 @@ for theme in themes:
 					with codecs.open('bijsluiters/%s.html' % (id_), 'w', encoding='utf8') as g:
 						g.write(map_info)
 
-				row = [name, summary, theme_name, theme_summary, subject, type_, data_owner, service_type, data_url, map_url, layer]
+				row = [name, summary, theme_name, subject, data_owner, data_url, data_url_error, map_url, layer]
 				writer.writerow(row)
 
 			else:
